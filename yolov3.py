3dcda1bb32d3f9cfc5c65ecd4d161b17b3ef425e
zCompKid <getintouchwithkal@gmail.com>
2019-07-30 22:23:57 +0300

    separated into functions

--- yolov3.py
+++ yolov3.py
@@ -14,12 +14,18 @@ import pandas as pd
 import os, json
 import argparse
 
-
+# Debugging flag: display results if true
 debug_flag = False
 
+# Score Threshold
+scoreThresh = 0.5
+
+# size of the input layer 
 width = 608
 height = 608
+
 inputs = tf.placeholder(tf.float32, [None, width, height, 3])
+
 model = nets.YOLOv3COCO(inputs, nets.Darknet19)
 #model = nets.FasterRCNN(inputs)
 #model = nets.YOLOv2(inputs, nets.Darknet19)
@@ -54,6 +60,52 @@ if args.debug:
     debug_flag = True
 ## Fetch all jpegs directories inside the route directory
 jpeg_dirs = [(pos_dir).replace("-jpegs", "") for pos_dir in os.listdir(dataset_dir) if pos_dir.endswith('-jpegs')]
+
+## Function to write list_json to list_file 
+def writeJSON(list_json):
+    parsed_list = json.loads(pd.Series(list_json).to_json(orient='values'))
+    list_file.write(json.dumps(parsed_list, indent=4))
+    list_json = []
+
+## Function to process and store in list_json
+def processBBoxes(frame, boxes1, width_vid, height_vid):
+    
+    res_img = cv2.resize(frame,(int(width_vid),int(height_vid)))
+            
+    for j in list_of_classes:
+        count = 0
+        if str(j) in classes:
+            lab = classes[str(j)]
+        if len(boxes1) != 0:                
+            for i in range(len(boxes1[j])):
+                box = boxes1[j][i] 
+                if boxes1[j][i][4] >= scoreThresh:    
+                    count += 1 
+                    Rx = width_vid/width
+                    Ry = height_vid/height   
+                    new_box = [0,0,0,0]
+                    new_box[0] = int(Rx * box[0])
+                    new_box[1] = int(Ry * box[1])
+                    new_box[2] = int(Rx * box[2])
+                    new_box[3] = int(Ry * box[3])
+
+                    json_object = { 'seqNumber': frameCount,
+                                    'class': lab,
+                                    'x': new_box[0],
+                                    'y': new_box[1],
+                                    'width': new_box[2] - new_box[0],
+                                    'height': new_box[3]- new_box[1],
+                                    'confidence': boxes1[j][i][4]
+                                }
+
+                    list_json.append(json_object)
+                    if debug_flag:
+                        cv2.rectangle(res_img,(new_box[0], new_box[1]),(new_box[2],new_box[3]),(0,255,0),1)
+                        cv2.rectangle(img,(box[0],box[1]),(box[2],box[3]),(0,255,0),1)
+
+                        cv2.putText(img, lab, (box[0],box[1]), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 255), lineType=cv2.LINE_AA)
+                        cv2.putText(res_img, lab, (new_box[0],new_box[1]), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 255), lineType=cv2.LINE_AA)
+    return res_img
 
 with tf.Session() as sess:
     ## Run the pretrained model
@@ -92,41 +144,7 @@ with tf.Session() as sess:
 
             boxes1 = np.array(boxes)
 
-            res_img = cv2.resize(frame,(int(width_vid),int(height_vid)))
-            
-            for j in list_of_classes:
-                count = 0
-                if str(j) in classes:
-                    lab = classes[str(j)]
-                if len(boxes1) != 0:                
-                    for i in range(len(boxes1[j])):
-                        box = boxes1[j][i] 
-                        if boxes1[j][i][4]>=.5:    
-                            count += 1 
-                            Rx = width_vid/width
-                            Ry = height_vid/height   
-                            new_box = [0,0,0,0]
-                            new_box[0] = int(Rx * box[0])
-                            new_box[1] = int(Ry * box[1])
-                            new_box[2] = int(Rx * box[2])
-                            new_box[3] = int(Ry * box[3])
-
-                            json_object = { 'seqNumber': frameCount,
-                                            'class': lab,
-                                            'x': new_box[0],
-                                            'y': new_box[1],
-                                            'width': new_box[2] - new_box[0],
-                                            'height': new_box[3]- new_box[1],
-                                            'confidence': boxes1[j][i][4]
-                                        }
-
-                            list_json.append(json_object)
-                            if debug_flag:
-                                cv2.rectangle(res_img,(new_box[0], new_box[1]),(new_box[2],new_box[3]),(0,255,0),1)
-                                cv2.rectangle(img,(box[0],box[1]),(box[2],box[3]),(0,255,0),1)
-    
-                                cv2.putText(img, lab, (box[0],box[1]), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 255), lineType=cv2.LINE_AA)
-                                cv2.putText(res_img, lab, (new_box[0],new_box[1]), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 255), lineType=cv2.LINE_AA)
+            res_img = processBBoxes(frame, boxes1, width_vid, height_vid)
 
             if debug_flag:
                 cv2.imshow("res_image", res_img)
@@ -134,8 +152,7 @@ with tf.Session() as sess:
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break          
         # Write JSON
-        parsed_list = json.loads(pd.Series(list_json).to_json(orient='values'))
-        list_file.write(json.dumps(parsed_list, indent=4))
-        list_json = []
+        writeJSON(list_json)
+        
 cap.release()
 cv2.destroyAllWindows()    
